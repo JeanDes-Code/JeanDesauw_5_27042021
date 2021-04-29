7d55ae0e57224d75e3848b27bbb6d78a8f4e40de
JeanDesauw <desauwjean@gmail.com>
2021-04-29 15:34:15 +0200

    Affichage dynamique des produits Via Javascript.

--- scripts/script.js
+++ scripts/script.js
@@ -1,66 +1,47 @@
-
-/*
-**Construction en cours 
-
-
 let store = document.getElementById('store');
-let products;
+
+//travail sur l'affichage dynamique depuis le serveur//
 
 
-const fetchProducts = async() => {
-    products = await fetch('http://localhost:3000/api/furniture')
-    .then(res => res.json());
+const getProducts = async function(){
+    let response = await fetch ("http://localhost:3000/api/furniture")
+    let products = await response.json();
+    /*for (let i in products){
+        console.log(products[i].imageUrl)
+        console.log(products[i].name)
+        console.log(products[i]._id);
+        console.log(products[i].price);
+        console.log(products[i].description);
+        for(let j in products[i].varnish){
+            console.log(products[i].varnish[j]);
+        }
+    }*/
+    return products;
+}
+
+const showProduct =async ()=> {
+    let products = await getProducts();
+    for(let i in products){
+        store.innerHTML +=(
+                `
+            <a class="productItem">
+                <img class="productImg" src=${products[i].imageUrl} />
+                <h2 class="productName descriptionStyle">${products[i].name}</h2>
+                <div class="productInfo ">
+                    <p class="productId">(Référence : ${products[i]._id})</p>
+                    <p class="productDescription descriptionStyle">${products[i].description}</p>
+                    <p class="productPrice descriptionStyle">${products[i].price} €</p>
+                </div>
+            </a>
+                `
+        )
+    }
 }
 
 
-const showProducts = async() => {
-    await fetchProducts();
-
-    store.innerHTML =(
-        products
-            .filter(product => product.name)
-            .map(product => (
-                `
-                    <a class="productItem">
-                        <img class="productImg" src="${product.imageUrl}" />
-                        <h2 class="productName">${product.name}</h2>
-                        <div class="productInfo">
-                            <p class="productId">Reference : ${product._id}</p>
-                            <p class="productDescription">${product.description}</p>
-                            <p class="productPrice">${product.price} EUR</p>
-                        </div>
-                        <select class="varnishSelect">
-                            <optgroup label="Vernis">
-                                <option>${product.varnish}</option>
-                            </optgroup>
-                        </select>
-                    </a>
-                `
-            ))
-            .join('')
-    );
-};
-
-**
-*/
+showProduct();
 
 
 
-let store = document.getElementById('store');
-let numberOfProducts = 5;
 
-for (let i=0; i<numberOfProducts; i++) {
-    store.innerHTML +=
-    `
-    <a class="productItem">
-        <img class="productImg" src="../images/oak_1.jpg" />
-        <h2 class="productName descriptionStyle">TITRE</h2>
-        <div class="productInfo descriptionStyle">
-            <p class="productId">(Reference : 645322324445)</p>
-            <p class="productDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <p class="productPrice">7500 EUR</p>
-        </div>
-    </a>
-    `;
-    
-};
+
