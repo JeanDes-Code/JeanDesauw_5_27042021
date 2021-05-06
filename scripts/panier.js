fcb0346a59fc956efefc329ebcdd2a6d307d60cd
JeanDesauw <desauwjean@gmail.com>
2021-05-06 19:11:45 +0200

    Début du travail sur le formulaire de commande

--- scripts/panier.js
+++ scripts/panier.js
@@ -4,9 +4,9 @@ let panierTotal = document.getElementById("panierTotal");
 let totalPrice=0;
 let totalNumber=0;
 
-console.log(storedPanier);
-
+/*Cette fonction construit le panier en récupérant les informations présente sur le local storage */
 const buildPanier =() =>{
+    /*Si le panier est vide on affiche un message*/ 
     if (storedPanier === null || storedPanier === []){
         panierTotal.innerHTML = (
             `
@@ -26,7 +26,7 @@ const buildPanier =() =>{
                 </div>
                 `
             );
-    
+            /*On calcule les totaux : prix et nombre d'articles */
             totalPrice += parseInt(storedPanier[i].total,10);
             totalNumber += parseInt(storedPanier[i].quantity,10);
         }
@@ -45,6 +45,56 @@ const buildPanier =() =>{
 
 buildPanier();
 
+
+
+let placeOrder = document.getElementById('placeOrder');
+let getBack = document.getElementById('getBack');
+let form = document.getElementById('form');
+let panier= document.getElementById('panier');
+
+/* Cette fonction permet d'afficher le formulaire de commande mais seulement si le panier n'est pas vide */
+const formToggle= () => {
+    if (storedPanier === null || storedPanier === []){
+        alert("⚠️ Votre panier est vide ! ⚠️");
+    }else{
+        getBack.classList.remove("hidden");
+        form.classList.remove("hidden");
+        panier.classList.add("hidden");
+    }
+}
+
+placeOrder.addEventListener('click', formToggle);
+
+/* Cette fonction permet un retour en arrière lorsque l'utilisateur a ouvert le formulaire par erreur */
+const formDisable= ()=> {
+    panier.classList.remove("hidden");
+    form.classList.add("hidden");
+    getBack.classList.add("hidden");
+}
+
+
+getBack.addEventListener('click',formDisable);
+
+/*
+Cette fonction permettra d'envoyer le formulaire au serveur
+
+
+let submitBtn = document.getElementById('submitBtn');
+
+const submitOrder= () => {
+    fetch("API-url"), {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(order)
+        location.href = "recap.html";
+    }
+}
+
+submitBtn.addEventListener('click',submitOrder);
+*/
 
 //Fonction permettant de vider le panier
 const resetCart = () =>{
